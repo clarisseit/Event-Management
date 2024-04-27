0349f8c4f36fa6e186e750d9d37d777af7e6bf83
clarisseit <clarisse21445@gmail.com>
2024-04-27 23:30:54 +0200

    Event Management

--- api/server.jsx
+++ api/server.jsx
@@ -5,11 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+
 mongoose.connect('mongodb://localhost/event-management', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,7 +19,7 @@ db.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
